24a552277e5cadc6ab69095abc9445006abd984b
Charles Hart <cchart@gmail.com>
2013-09-21 22:41:53 +0000

    Ready to do Science.

--- coneSensor.py
+++ coneSensor.py
@@ -1,50 +1,53 @@
 #!/usr/bin/python
 
-import cv, serial, struct, time
+import cv, serial, struct, argparse
 from datetime import datetime
 
+
+# center and depth of the polar coordinate transform
 centerX = 160
 centerY = 120
+M = 69
 
+# define "orange" in a simple way
 lower = cv.Scalar(40, 100, 175) 
 upper = cv.Scalar(90, 180, 255)
 
-unwrapped = None
 
-startTime = datetime.now().time()
-
-cyril = serial.Serial('/dev/ttyAMA0', 9600) #open first serial port and give it a good name
-print "Opened "+cyril.portstr+" for serial access"
-
-def on_mouse(event, x, y, flags, param):
+# mouse event callback
+def on_mouse(event, x, y, flags, param): 
   if event==cv.CV_EVENT_LBUTTONDOWN:
-    print "clicked ", x, ", ", y, ": ", unwrapped[y,x]
-    #global centerX
-    #global centerY
-    #centerX = x
-    #centerY = y
+    print "clicked ", x, ", ", y, ": ", unwrapped[y,x] # get the color of the pixel (B,G,R)
 
-if __name__ == '__main__':
+
+# main program 
+if __name__ == '__main__': 
+  # First check to see if the program was invoked with any options  
+  parser = argparse.ArgumentParser(description='Measure bearings to orange landmarks.')
+  parser.add_argument("-g", "--gui", action="store_true", help="Show video output") 
+  args = parser.parse_args()
+  
+  # open first serial port and give it a good name
+  cyril = serial.Serial('/dev/ttyAMA0', 9600) 
+  #print "Opened "+cyril.portstr+" for serial access"
+  
+  # open the first video4linux device with openCV, and ask it to be 320x240
   capture = cv.CaptureFromCAM(0)
   cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 320)
   cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
   
+  # Define some pixel canvases in memory
   img = cv.CreateImage((320, 240), 8, 3)
   polar = cv.CreateImage((360, 360), 8, 3)
   unwrapped = cv.CreateImage((360, 40), 8, 3)
   cones = cv.CreateImage((360, 40), 8, 1)
 
-  cv.NamedWindow('cam')
-  cv.ResizeWindow('cam', 320,240)
-
-  cv.NamedWindow('unwrapped')
-  cv.ResizeWindow('unwrapped', 360,40)
-  cv.NamedWindow('cones')
-  
-  cv.SetMouseCallback('unwrapped', on_mouse)
-  #on_mouse(cv.CV_EVENT_LBUTTONDOWN, img.width/2, img.height/2, None, None)
-
-  M = 69
+  if args.gui:
+    #cv.NamedWindow('cam')
+    cv.NamedWindow('unwrapped')
+    cv.NamedWindow('cones')
+    cv.SetMouseCallback('unwrapped', on_mouse)
+    #on_mouse(cv.CV_EVENT_LBUTTONDOWN, img.width/2, img.height/2, None, None)
 
   while True:
     # wait for next frame from camera
@@ -67,13 +70,14 @@ if __name__ == '__main__':
     cv.Dilate(cones, cones, k) 
     
     # Display the images from the three major steps of acquisition, transformation, and segmentation
-    cv.ShowImage('cam', img)
-    cv.ShowImage('unwrapped', unwrapped)
-    cv.ShowImage('cones', cones)
+    if args.gui:
+      #cv.ShowImage('cam', img)
+      cv.ShowImage('unwrapped', unwrapped)
+      cv.ShowImage('cones', cones)
 
     size = 0
     segments = 0
-    print "found %d" % (segments),
+    #print "found %d" % (segments),
     bearings = []
     for i in xrange(360-2):
       # examine the current and next bearing's measurements
@@ -85,11 +89,11 @@ if __name__ == '__main__':
       elif (cur == 255 and nex == 0):
         # a segment of beacon has ended
         segments = segments + 1
-        print segments,
+        #print segments,
         bearings.append([int(i-(size/2)), size])
         size = 0
-      if (i == 360-2-1 and size != 0): #handle wraparound
-        if (cones[0,0] == 255): #if the first pixel is 'on', there is wraparound.
+      if (i == 360-2-1 and size != 0): # handle wraparound
+        if (cones[0,0] == 255): # if the first pixel is 'on', there is wraparound.
           if (bearings[0][1] > size): # will the new bearing fall on the 0 side (not the 360 side)?
             bearings[0] = [(((size+bearings[0][1])/2)-size), size+bearings[0][1]]
           else:
@@ -97,13 +101,19 @@ if __name__ == '__main__':
             bearings.pop(0)
         else: # there is no wraparound, just end the segment
           segments = segments + 1
-          print segments,
+          #print segments,
           bearings.append([int(i-(size/2)), size])
-    print "segments: %s" % bearings
+    #print "segments: %s" % bearings
 
-    #tell the cockroach what it wants to hear
+    # now that the measurements have been made, arrange them for logging
+    bearingstring = ""
+    for b in bearings:
+      bearingstring = bearingstring + "%03d," % b[0]
+
+    # tell the cockroach what it wants to hear
     cyril.write(struct.pack('cbbb', '\xfa', 0, 111, 0))
 
+    # Data Logging with POSIX stdio!
     if (cyril.inWaiting() > 0):
       logdata = cyril.read(cyril.inWaiting())
       a = 0
@@ -111,10 +121,12 @@ if __name__ == '__main__':
       for c in logdata:
         if c == '\n':
           now = datetime.now().time()
-          print "%02d, %02d, %02d, %02d," % (now.hour, now.minute, now.second, now.microsecond/10000),
-          print "%d," % (now.hour*360000 + now.minute*6000 + now.second*100 + now.microsecond/10000),
-          print logdata[a:b], ",", segments, ",", bearings
-
+          print "%02d,%02d,%02d,%03d,%d,%s,%s,%s" % \
+            (now.hour, now.minute, now.second, now.microsecond/1000, \
+            (now.hour*3600000 + now.minute*60000 + now.second*1000 + now.microsecond/1000), \
+            logdata[a:b-1], segments, bearingstring)
+          a = b + 1 # leapfrog the other counter every line
+        b = b + 1 # increment the counter every character
  
     key = cv.WaitKey(30)
     #print "key ",key
@@ -128,3 +140,4 @@ if __name__ == '__main__':
         M = (M - 5)%100
 
   cv.DestroyAllWindows()
+
